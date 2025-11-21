cdd0582b3da65dad7f0645d6ae5d3af4ebdf4b6c
Oscar Fong <hyfongoscar@gmail.com>
2025-11-21 19:55:58 +0800

    fix(vite): trailing slashes

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-export default defineConfig({
-  base: '/app',
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/app/' : '/app',
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   server: {
     port: 3000,
   },
-});
+}));
