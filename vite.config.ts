0eed7fd0674cd41e977815560a1393c97c1e8a5b
Oscar Fong <hyfongoscar@gmail.com>
2025-11-21 19:09:24 +0800

    fix(vite): recover app base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(({ mode }) => ({
-  base: '/',
+  base: mode === 'production' ? '/app/' : '/',
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   server: {
     port: 3000,
